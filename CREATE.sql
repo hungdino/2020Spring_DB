e648779776ec2a1fe5a64289378fce19dbedce8b
hungdino <hungdino8921@gmail.com>
2020-04-15 17:44:14 +0800

    Try script

---
+++ CREATE.sql
@@ -0,0 +1,15 @@
+create table champ(
+    champion_name varchar(15),
+    champion_id int default 0,
+    primary key (champion_id)
+);
+
+load data local infile './champs.csv'
+into table champ
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;
+
+
+
