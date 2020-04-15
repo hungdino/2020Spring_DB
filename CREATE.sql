e27912f19b1acf5e9913a7d5580147e8254ce3ed
hungdino <hungdino8921@gmail.com>
2020-04-15 17:54:15 +0800

    Try script

--- CREATE.sql
+++ CREATE.sql
@@ -1,10 +1,20 @@
 create table champ(
-    champion_name varchar(15),
-    champion_id int default 0,
+    champion_name varchar(15) NOT NULL,
+    champion_id int default 0 NOT NULL,
     primary key (champion_id)
 );
 
-load data local infile './champ.csv'
+create table match_info(
+    match_id int default 0 NOT NULL,
+    duration int default 0 ,
+    version varchar(15),
+    primary key (match_id)
+);
+
+
+
+
+load data local infile './champs.csv'
 into table champ
 fields terminated by ','
 enclosed by '"'
