93970faf1e1b7d1df285b1a45b8a72bcf54e4bd2
hungdino <hungdino8921@gmail.com>
2020-04-16 08:08:13 +0800

    test load file

--- CREATE.sql
+++ CREATE.sql
@@ -1,15 +1,27 @@
-create table champ(
+CREATE TABLE champ(
     champion_name varchar(15) NOT NULL,
     champion_id int default 0 NOT NULL,
-    primary key (champion_id)
+    PRIMARY KEY (champion_id)
 );
 
-create table match_info(
+CREATE TABLE match_info(
     match_id int default 0 NOT NULL,
     duration int default 0 ,
     version varchar(15),
-    primary key (match_id)
+    PRIMARY KEY (match_id)
 );
+
+CREATE TABLE participant(
+    player_id int default 0 NOT NULL,
+    match_id int default 0 NOT NULL,
+    player tinyint default 0,
+    champion_id int default 0 NOT NULL,
+    ss1 varchar(15),
+    ss2 varchar(15),
+    position varchar(13) NOT NULL,
+    PRIMARY KEY (player_id),
+    FOREIGN KEY (match_id) REFERENCES match_info(match_id)
+ );
 
 
 
@@ -18,7 +30,7 @@ load data local infile './champs.csv'
 into table champ
 fields terminated by ','
 enclosed by '"'
-lines terminated by '\n'
+lines terminated by '\r\n'
 ignore 1 lines;
 
 
